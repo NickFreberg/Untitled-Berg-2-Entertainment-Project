e5818d729ce9812f2b6da339e6b09c633c5c1598
RMalmberg <rmalmberg@student.bridgew.edu>
2019-11-03 20:49:07 -0500

    ~AnimatedWalkingSprite : Player implemented +Scott P sprite sheet ~Walks up, down. left, right

--- TiledMap.py
+++ TiledMap.py
@@ -26,7 +26,7 @@ class PlayerSprite(arcade.AnimatedWalkingSprite):
         return self.life
 
     def move(self, direction:MoveEnum):
-        #as a class exercise, lets fix this so it doesn't go off the window
+
         if direction == MoveEnum.UP:
             self.center_y += self.speed
         elif direction == MoveEnum.DOWN:
@@ -57,14 +57,28 @@ class MultiLayeredWindow (arcade.Window):
 
         playerPath = pathlib.Path.cwd() / 'Assets' / 'scottpilgrim.png'
         self.playerlist = arcade.SpriteList()
-        self.player = PlayerSprite(PLYR_MOVE_SPEED, 1, game_window=self )
+        self.player = PlayerSprite(PLYR_MOVE_SPEED, 1, game_window=self)
         self.player.center_x = 400
         self.player.center_y = 400
 
-        frame = arcade.load_texture(str(playerPath), 0, 0, 102, 120)
+        frame = arcade.load_texture(str(playerPath), 0, 0, height=140, width=108)
+        self.player.stand_right_textures = []
+        self.player.stand_right_textures.append(frame)
+        self.player.texture = frame
+
+        frame = arcade.load_texture(str(playerPath), 0, 140, height=140, width=108)
         self.player.stand_left_textures = []
         self.player.stand_left_textures.append(frame)
-        self.player.texture = frame
+
+        self.player.walk_right_textures = []
+        self.player.walk_left_textures = []
+        for image_num in range(8):
+            frame = arcade.load_texture(str(playerPath), image_num*108, 0, height=140, width=108)
+            self.player.walk_right_textures.append(frame)
+
+        for image_num in range(8):
+            frame = arcade.load_texture(str(playerPath), image_num*108, 140, height=140, width=108)
+            self.player.walk_left_textures.append((frame))
 
 
         self.playerlist.append(self.player)
@@ -73,27 +87,34 @@ class MultiLayeredWindow (arcade.Window):
        # self.maplist = arcade.tilemap.process_layer(sample__map, "map_layer", 1)
 
     def on_key_press(self, key: int, modifiers: int):
+        """ Movement"""
         if key == arcade.key.LEFT:
-            self.direction  = MoveEnum.LEFT
+            self.player.change_x = -PLYR_MOVE_SPEED
         elif key == arcade.key.RIGHT:
             self.player.change_x = PLYR_MOVE_SPEED
-    def on_key_release(self, key: int, modifiers: int):
+        elif key == arcade.key.UP:
+            self.player.change_y = PLYR_MOVE_SPEED
+        elif key == arcade.key.DOWN:
+            self.player.change_y = -PLYR_MOVE_SPEED
 
+    def on_key_release(self, key: int, modifiers: int):
+        """ Movement"""
         if self.player.change_x < 0 and key == arcade.key.LEFT:
             self.player.change_x = 0
         elif self.player.change_x > 0 and key == arcade.key.RIGHT:
             self.player.change_x = 0
+        elif self.player.change_y > 0 and key == arcade.key.UP:
+            self.player.change_y = 0
+        elif self.player.change_y < 0 and key == arcade.key.DOWN:
+            self.player.change_y = 0
 
     def on_draw(self):
         arcade.start_render()
         self.playerlist.draw()
-"""
+
     def on_update(self, delta_time: float):
         self.playerlist.update()
         self.playerlist.update_animation()
-"""
-
-
 
 def main():
     """The Main Method"""
